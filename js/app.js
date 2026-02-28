08f40398e74e1d1a7b0fd2421d555e81a56f9977
MaiBii <maibii18082001@gmail.com>
2026-02-28 14:44:57 +0900

    part 1 weather

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
-var search = document.querySelector(".search");
-var city = document.querySelector(".city");
-var country = document.querySelector(".country");
-var value = document.querySelector(".value");
-var shortDesc = document.querySelector(".short-desc");
-var visibility = document.querySelector(".visibility");
-var wind = document.querySelector(".wind span");
-var sun = document.querySelector(".sun span");
-var value = document.querySelector(".value span");
+var search = document.querySelector(".search")
+var city = document.querySelector(".city")
+var country = document.querySelector(".country")
+var value = document.querySelector(".value")
+var shortDesc = document.querySelector(".short-desc")
+var visibility = document.querySelector(".visibility")
+var wind = document.querySelector(".wind span")
+var sun = document.querySelector(".sun span")
+var value = document.querySelector(".value span")
+var time= document.querySelector(".time")
+var content=document.querySelector(".content")
 
 async function changeWeatherUi() {
-  let capitalSearch = search.value.strim();
-  let apiURL = `search.value.trim(https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=2df2c4c8a2caaa752c1e878517a359d6)`;
-  fetch(apiURL).then((res) => res.json());
-  console.log(Data);
-  city.innerText = data.name;
-  country.innerText = data.sys.country;
-  visibility.innerText = data.visibility + "m";
-  wind.innerText = data.wind.speed + "m/s";
-  sun.innerText = data.main.humidity + "%";
-  value.innerText = data.main.temp - 273.15;
-  shortDesc.innerText = data.weather.main;
-  time.innerText = new Date().toLocaleDateString("vi");
+let capitalSearch = search.value.strim()
+let apiURL = `search.value.trim(https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=cb8d857e952868047d1a7a90b239fba`;
+fetch(apiURL).then((res) => res.json())
+if(data.cod==200){
+   content.classList.remove('hide')
+   city.innerText = data.name
+   country.innerText = data.sys.country
+   visibility.innerText = data.visibility + "m"
+   wind.innerText = data.wind.speed + "m/s"
+   sun.innerText = data.main.humidity + "%"
+   value.innerText = Math.round((data.main.temp - 273.15))+'oC'
+   shortDesc.innerText = data.weather[0]?data.weather[0].main=''
+   time.innerText = new Date().toLocaleDateString("jp")
+}else{
+   content.classList.add('hide')
 }
-changeWeatherUi;
+}
+
+search.addEventListener('keypress',function(e){
+   if(e.code==='Enter'){
+      changeWeatherUi()
+   }
+})
